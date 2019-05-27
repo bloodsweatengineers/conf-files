a844c314457e0e6747c2b629a56323644f53c3dd
folkert <faj.kevelam@gmail.com>
2019-05-27 18:29:45 +0200

    Initial commit

--- install.sh
+++ install.sh
@@ -1,3 +1,162 @@
 #!/bin/bash
 
-bash vim/install_vim.sh
+VERSION="Configuration installation ver-0.0.0"
+USER=`id -u -n`
+CURRENT_DIR=`pwd`
+
+VERBOSE=0
+BUILD_DIR="build/"
+CONF_ONLY=0
+ROOT=0
+USER_HOME=
+
+function Success() {
+	echo -e "\e[32mSuccess:\t$1\e[39m"
+}
+
+function Info() {
+	echo -e "\e[33mInfo:\t$1\e[39m"
+}
+
+function Error() {
+	echo -e "\e[31mError:\t$1\e[39m"
+}
+
+function Fatal() {
+	Error "$1"
+	Error "Exiting"
+}
+
+function Echo_If_Verbose() {
+	if [[ $VERBOSE -eq 1 ]]; then
+		case "$1" in
+			success )
+				Success "$2"
+				;;
+			info )
+				Info "$2"
+				;;
+			error )
+				Info "$2"
+				;;
+			fatal )
+				Info "$2"
+				;;
+		esac
+	fi
+}
+
+source ./vim/install_vim.sh
+#source ./vim/install_awesome.sh
+
+function Vim_Installation() {
+	Info "Starting vim installation"
+	Echo_If_Verbose info "Cleaning up old vim installations"
+	if Vim_Cleanup; then
+		Echo_If_Verbose success "Cleaned up old vim installations"
+	else
+		Fatal "Failed to clean up old vim installations"
+		exit
+	fi
+
+	Echo_If_Verbose info "Installing dependencies of vim"
+	if Vim_Install_Dependencies; then
+		Echo_If_Verbose success "Installed dependencies"
+	else
+		Fatal "Failed to install dependencies of vim"
+		exit
+	fi
+
+	Echo_If_Verbose info "Building vim"
+	if Vim_Build $BUILD_DIR $VIM_SHARE_DIR; then
+		Echo_If_Verbose success "Build vim"
+	else
+		Fatal "Failed to Build vim"
+		exit
+	fi
+
+	Echo_If_Verbose info "Installing vim"
+	if ! Vim_Install; then
+		Fatal "Failed to install vim"
+		exit
+	fi
+
+	Echo_If_Verbose info "Setting alternatives"
+	if Vim_Set_Alternatives; then
+		Echo_If_Verbose success "Set alternatives"
+	else
+		Error "Failed to set alternatives"
+	fi
+
+	Success "Installed vim"
+}
+
+function Vim_Plugin_Installation() {
+	Info "Moving vim config"
+	Vim_Move_Config $CURRENT_DIR $USER_HOME $USER
+	Echo_If_Verbose success "Moved vim config"
+	Info "Installing Plugins"
+	Vim_Install_Plugins $USER_HOME $USER
+	Success "Installed Plugins"
+}
+
+while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do 
+	case $1 in
+		-V | --version )
+			echo $VERSION
+			exit
+			;;
+		-v | --verbose )
+			VERBOSE=1 
+			;;
+		--build-dir )
+			shift;
+			BUILD_DIR=$1
+			;;
+		--conf-only )
+			CONF_ONLY=1
+			;;
+		--root )
+			ROOT=1
+			;;
+	esac; shift;
+done
+
+if [[ $CONF_ONLY -eq 0 ]]; then
+	if [[ "$USER" == "root" && -z $SUDO_USER && $ROOT -eq 0 ]]; then
+		Fatal "This script needs to be run with sudo or with --root."
+		exit;
+	elif [[ "$USER" != "root" ]]; then
+		Fatal "This script needs some elevated privileges."
+		exit;
+	fi
+fi
+
+if [[ $ROOT -eq 1 ]]; then
+	USER_HOME=~
+else
+	USER_HOME="/home/$SUDO_USER"
+	USER=$SUDO_USER
+fi
+
+if [[ $VERBOSE -eq 1 ]]; then
+	Info "Build directory\t: $BUILD_DIR"
+	Info "Home directory\t: $USER_HOME"
+	if [[ $CONF_ONLY -eq 1 ]]; then
+		Info "Running configuration only"
+	fi
+	if [[ $ROOT -eq 1 ]]; then
+		Info "Forcibly running root configuration"
+	fi
+fi
+
+if [[ $CONF_ONLY -eq 0 ]]; then
+	Vim_Installation
+	Vim_Plugin_Installation
+else
+	Vim_Plugin_Installation
+fi
+
+Info "Cleaning up"
+rm -rf "$CURRENT_DIR/$BUILD_DIR"
+Success "Done. Exiting"
--- vim/install_vim.sh
+++ vim/install_vim.sh
@@ -7,46 +7,19 @@ VIM_DEPENDECIES="libncurses5-dev libgnome2-dev libgnomeui-dev libgtk2.0-dev \
 
 OLD_VIM_INSTALLATIONS="vim vim-runtime gvim vim-tiny vim-common vim-gui-common vim-nox"
 
-function Vim_Install_Dependencies() {
-	apt-get install $VIM_DEPENDECIES
-	return 0
-}
-
-function Vim_Remove_Old_Vim() {
+function Vim_Cleanup() {
 	apt-get remove $OLD_VIM_INSTALLATIONS
 	return 0
 }
 
-function Error() {
-	echo -e "\e[31m$1\e[39m"
-}
-
-function Info() {
-	echo -e "\e[33m$1\e[39m"
-}
-
-function Success() {
-	echo -e "\e[32m$1\e[39M"
+function Vim_Install_Dependencies() {
+	apt-get install $VIM_DEPENDENCIES
+	return 0
 }
 
 function Vim_Build() {
-
-	Info "Installing dependencies for vim"
-	if vim_install_dependencies; then
-		Success "Successfully installed dependencies"
-	else
-		Error "Failed to install dependencies"
-		return 1
-	fi
-
-	if Vim_Remove_Old_Vim; then
-		Success "Successfully removed old vim installation"
-	else
-		Error "Failed to remove old vim installation"
-		return 1
-	fi
-
-	cd $1
+	mkdir -p "$1"
+	cd "$1"
 	git clone https://github.com/vim/vim.git
 	cd vim
 	make distclean
@@ -62,37 +35,33 @@ function Vim_Build() {
 				--enable-gui=auto \
 				--enable-cscope \
 				--prefix=/usr/local
-	make VIMRUNTIMEDIR=$2 -j 4
-	make install
+	make VIMRUNTIMEDIR="$2" -j 4
+	return 0
+}
 
+function Vim_Install() {
+	make install
+	return 0
+}
+
+function Vim_Set_Alternatives() {
 	update-alternatives --install /usr/bin/editor editor /usr/local/bin/vim 1
 	update-alternatives --set editor /usr/local/bin/vim
 	update-alternatives --install /usr/bin/vi vi /usr/local/bin/vim 1
 	update-alternatives --set vi /usr/local/bin/vim
+	return 0
+}
 
-	Success "Installed vim"
+function Vim_Move_Config() {
+	rm -rf "$2/.vimrc"
+	cp "$1/vim/.vimrc" "$2/.vimrc"
+	chown $3:$3 "$2/.vimrc"
 }
 
 function Vim_Install_Plugins() {
-	rm -rf /home/$SUDO_USER/.vim/
-	rm -rf /home/$SUDO_USER/.vimrc
-
-	cp vim/.vimrc /home/$SUDO_USER/.vimrc
-	sudo -u $SUDO_USER git clone https://github.com/VundleVim/Vundle.vim.git /home/$SUDO_USER/.vim/bundle/Vundle.vim
-	sudo -u $SUDO_USER vim +PluginInstall +qall
+	rm -rf "$2/.vim/"
+	git clone https://github.com/VundleVim/Vundle.vim.git "$1/.vim/bundle/Vundle.vim"
+	chown -R $2:$2 "$1"
+	vim +PluginInstall +qall
 	python3 ~/.vim/bundle/YouCompleteMe/install.py --clang-completer
 }
-
-
-Info "Trying to install vim and plugins..."
-
-VIM_DIR=`which vim`
-
-if [[ -z "$VIM_DIR" ]]; then
-	Error "Vim not found, starting installation..."
-	Vim_Install ~ /usr/local/share/vim/vim81
-	Vim_Install_Plugins
-elif [[ -n "$VIM_DIR" ]]; then
-	Success "Vim found, installing plugins..."
-	Vim_Install_Plugins
-fi
